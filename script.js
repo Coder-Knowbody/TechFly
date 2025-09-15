3518b5952d66ec4f291bd53fd20eded59974bc1d
Coder-Knowbody <abdulroheem_01@proton.me>
2025-09-15 15:38:17 +0100

    Finished with the whole website

---
+++ script.js
@@ -0,0 +1,10 @@
+function displayOn() {
+  document.getElementById("nav-text-id").style.display = "block";
+  document.getElementById("ham-button").style.display = "none";
+  document.getElementById("close-button").style.display = "block";
+}
+function displayOff() {
+  document.getElementById("nav-text-id").style.display = "none";
+  document.getElementById("ham-button").style.display = "block";
+  document.getElementById("close-button").style.display = "none";
+}
